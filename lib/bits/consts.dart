96732c0bf454095d48806c4644889dd9ae55d4a5
Jack Meng <jackmeng0814@gmail.com>
2023-10-26 07:38:24 -0400

    `add` revert previous color change

--- lib/bits/consts.dart
+++ lib/bits/consts.dart
@@ -9,9 +9,9 @@ final class LaF {
   static const Color empty = Color.fromARGB(0, 0, 0, 0);
   static const Color primaryBackground =
       Color.fromARGB(255, 247, 237, 225);
-  static const Color primaryColor = Color.fromARGB(255, 79, 225, 72);
+  static const Color primaryColor = Color.fromARGB(255, 225, 164, 72);
   static const Color primaryColorTint =
-      Color.fromARGB(255, 68, 197, 124);
+      Color.fromARGB(255, 236, 160, 72);
   static const Color primaryColorTintDarker_Pressed =
       Color.fromARGB(180, 199, 144, 81);
   static const Color primaryColorFgContrast =
